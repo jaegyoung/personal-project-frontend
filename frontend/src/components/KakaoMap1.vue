95e3a20a0af749cc4c94b45de1b0ad9c29044fb3
Jaekyoung Kim <88273159+jaegyoung@users.noreply.github.com>
2023-07-10 15:52:13 +0900

    feat:카카오 지도 구현 하기(1) [WPF-7]
    네이버 말고 카카오를 사용하기 위해 지도를 구현하는 중, vue 패키지 중에 vue-daum-map이 있어서 사용했는데 구 버전인 것 같아. 사용하지 않고 지도 구현 중.

---
+++ frontend/src/components/KakaoMap1.vue
@@ -0,0 +1,40 @@
+<template>
+    <div id="app">
+      <div id="map"></div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+      mounted() {
+          if (window.kakao && window.kakao.maps) {
+              this.initMap();
+          } else {
+              const script = document.createElement('script');
+              /* global kakao */
+              script.onload = () => kakao.maps.load(this.initMap);
+              script.src = 'http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=185c27ae2c3a87993d3cf6235d2507ba';
+              document.head.appendChild(script);
+          }
+      },
+      methods: {
+          initMap() {
+              let container = document.getElementById('map');
+              let options = {
+                center: new kakao.maps.LatLng(37.649734,127.114739,),
+                level: 3
+              };
+  
+              let map = new kakao.maps.Map(container, options);
+              map.setMapTypeId(kakao.maps.MapTypeId.ROADMAP);
+          }
+      }
+  }
+  </script>
+  
+  <style>
+  #map {
+      width: 800px;
+      height: 600px;
+  }
+  </style>
