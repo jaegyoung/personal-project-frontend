04d076ab9cf6ed2069cf5c2c2af9b8ae97faf7d6
Jaekyoung Kim <88273159+jaegyoung@users.noreply.github.com>
2023-07-14 11:42:30 +0900

    feat: 회원 가입시 사용할 주소 api exampleAddress 폼 생성[WPF-14]

--- frontend/src/router/account/index.js
+++ frontend/src/router/account/index.js
@@ -4,6 +4,7 @@ import VueRouter from 'vue-router'
 import AccountRegisterPage from '@/views/account/AccountRegisterPage.vue'
 import HomeView from '@/views/HomeView.vue'
 import AccountLoginPage from '@/views/account/AccountLoginPage.vue'
+import exampleAddress from '@/views/account/address/exampleAddress.vue'
 
 
 Vue.use(VueRouter)
@@ -24,6 +25,11 @@ const accountRoutes = [
     name: 'AccountLoginPage',
     component: AccountLoginPage
 
+  },
+  {
+    path:'/exampleAddress',
+    name:'exampleAddress',
+    component: exampleAddress
   }
 ]
 
---
+++ frontend/src/views/account/address/exampleAddress.vue
@@ -0,0 +1,39 @@
+<template lang="">
+    <div>
+       <form> 
+        <input type="text" placeholder="우퍈반허" v-model="zonecode" readonly/>
+        <v-btn id="postcode" @click="openPostcode">검색</v-btn><br/>
+        <input type="text" v-model="roadAddress" placeholder="주소" readonly/><br/>
+        <input type="text" v-model="detailAddress" placeholder="상세주소"/>
+        <v-btn @click="showAddress">주소 보여줘!</v-btn>
+        <span v-if="totalAddress!=null">{{totalAddress}}</span>
+       </form></div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            zonecode: '',
+            roadAddress:'',
+            detailAddress:'',
+            totalAddress:null
+        }
+    },
+    methods: {
+        openPostcode(){
+            new window.daum.Postcode({
+                oncomplete: (data)=>{
+                    this.zonecode= data.zonecode;
+                    this.roadAddress=data.roadAddress;
+                },
+            }).open()
+        },
+        showAddress(){
+            this.totalAddress=this.zonecode+this.roadAddress
+        }
+    },
+}
+</script>
+<style lang="">
+    
+</style>
